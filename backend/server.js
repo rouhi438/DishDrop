9eb351e258ea0327b98775e5957ff35a1f116c61
Abbas Rouhi <rouhi438@gmail.com>
2026-06-09 14:39:46 +0200

    add new path to check mobile login

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,23 @@ const allowedOrigin = [
   "https://dishdrop.vercel.app",
   "https://dishdrop-8fqc.onrender.com",
 ];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     credentials: true,
+//   }),
+// );
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*",
     credentials: true,
   }),
 );
@@ -32,6 +38,10 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/", authRoutes);
 app.use("/recipes", recipeRoutes);
 app.use("/admin", adminRoutes);
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong", timestamp: new Date().toISOString() });
+});
 
 mongoose
   .connect(process.env.MONGO_URL)
